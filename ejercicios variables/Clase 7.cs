af23e102b76cc25124a18714bd941a0955f8c203
Joseca94 <alumno@viralgamingacademy.com>
2020-03-09 14:39:44 +0100

    Clase 7

---
+++ ejercicios variables/Clase 7.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace practicas
+{   
+
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+            // PAR IMPAR CON ARRAY 
+
+
+            //int[] numbers = new int[] { 2, 2, 4, 5, 7, 9, };
+            //int par = 0;
+            //int impar = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        par++;
+            //    }
+            //    else
+            //    {
+            //        impar++;
+            //    }
+
+            //} 
+            //Console.WriteLine("pares: " + par);
+            //Console.WriteLine("impares: " + impar);
+
+
+
+
+
+
+
+
+            ////PAR IMPAR CON LISTA Y FOR                     
+
+            //List<int> listpar = new List<int>();
+            //List<int> listimpar = new List<int>();
+            //Console.Write("Cuantos numeros quieres?");
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write("introduce un numero: ");
+            //    int number = int.Parse(Console.ReadLine());
+            //    if (number % 2 == 0)
+            //    {
+            //        listpar.Add(number);
+            //    }
+            //    else
+            //    {
+            //        listimpar.Add(number);
+            //    }
+            //}
+            //    Console.Write("numeros pares: ");
+            //    for (int i = 0; i < listpar.Count; i++)
+            //    {
+            //        Console.Write(listpar[i] + " ");
+            //    }
+            //    Console.WriteLine();
+            //    Console.Write("numeros impares : ");
+            //    for (int i = 0; i < listimpar.Count; i++)
+            //    {
+            //    Console.Write(listimpar[i] + " ");
+            //    };
+
+
+
+
+
+
+
+            // TABLA DE MULTIPLICAR
+
+            //Console.Write("Escribe el numero que quieres multiplicar: ");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] numbers = new int[10];
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    numbers[i] = n * (i + 1);
+
+            //}
+            //Console.WriteLine("la tabla del " + n + " es:");
+            //for (int y = 0; y < numbers.Length; y++)
+            //{
+            //    Console.WriteLine(numbers[y]);
+            //}
+
+
+
+
+
+        }
+    }
+
+    
+}
