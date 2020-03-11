dd268f00594b046eb115971627116429f05665cb
Joseca94 <alumno@viralgamingacademy.com>
2020-03-11 11:39:12 +0100

    Create clase 8.cs

---
+++ ejercicios variables/clase 8.cs
@@ -0,0 +1,244 @@
+﻿using System;
+
+namespace clase_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            // MOSTRAR DEL 1 AL 10 
+
+
+            //int i = 0;
+            //while (i < 10)
+            //{
+            //    Console.WriteLine(i + 1);
+            //    i++;
+
+            //}
+            //i = 0;
+            //do
+            //{
+            //    Console.WriteLine(i + 1);
+            //    i++;
+
+            //} while (i < 10);
+            //for ( i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i + 1);
+
+
+            //}
+
+            //SUMA DE 10 NUMEROS 
+
+
+            //int i = 0;
+            //int total = 0;
+            //while (i < 10)
+            //{
+            //    Console.WriteLine(i + 1);
+            //    i++;
+            //    total += i;
+
+            //}
+            //Console.WriteLine(total);
+            //i = 0;
+            //total = 0;
+            //do
+            //{
+            //    Console.WriteLine(i + 1);
+            //    i++;
+            //    total += i;
+
+            //} while (i < 10);
+            //Console.WriteLine(total);
+            //for (i = 0, total = 0; i < 10; i++, total += i)
+            //{
+            //    Console.WriteLine(i + 1);
+            //}
+            //Console.WriteLine(total);
+
+
+            // escribe un numero y te suma lasa tepeticiones o yo que se
+
+            //Console.Write("introduce un numero: ");
+            //int n = int.Parse(Console.ReadLine());
+            //int i = 0;
+            //int total = 0;
+            //while (i < n)
+            //{
+            //    Console.WriteLine(i + 1);
+            //    i++;
+            //    total += i;
+
+            //}
+            //Console.WriteLine(total);
+            //i = 0;
+            //total = 0;
+            //do
+            //{
+            //    Console.WriteLine(i + 1);
+            //    i++;
+            //    total += i;
+
+            //} while (i < n);
+            //Console.WriteLine(total);
+            //for (i = 0, total = 0; i < n; i++, total += i)
+            //{
+            //    Console.WriteLine(i + 1);
+            //}
+            //Console.WriteLine(total);
+
+
+
+
+
+
+
+
+            ////SUMA DE 10 NUMEROS QUE EL USUARIO PONGA           
+            //int x = 0;
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("introduce un numero");
+            //    x += int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("la suma es: " + x);
+
+
+
+
+
+
+            // EL CUBO DEL LOS NUMEROS QUE PIDAS
+
+            //Console.WriteLine("introduce un numero");
+            //int x = int.Parse(Console.ReadLine());               
+            //for (int i = 1; i <= x; i++)
+            //{
+
+            //    Console.WriteLine("El cubo es =  " + (i * i * i));
+            //}
+
+
+
+
+            // TABLA DE MULTIPLICAR
+            //Console.Write("Escribe un numero: ");
+            //int tabla = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Escribe otro numero: ");
+            //int Numero = int.Parse(Console.ReadLine());
+            //int contador = 0;
+            //while (contador <= Numero)
+            //{
+            //    Console.WriteLine(tabla + "x" + contador + "=" + (tabla * contador));
+            //    contador += 1;
+
+            //}            
+            //Console.Write("Escribe un numero: ");
+            //int tabla = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i + "x" + tabla + "=" + (i * tabla));
+
+            //}
+
+
+
+
+            // MOSTRAR IMPARES HASTA EL NUMERO QUE DIGA EL USUARIO
+            //Console.WriteLine("Escribe un numero maximo:  ");
+            //int max = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= max; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+
+            //    }
+            //IMPARES SIMPLIFICADO
+            //int n = 100;
+            //for (int i = 1; i <= n; i+= 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+
+
+            // EJERCICIO DE PATRONES
+
+            //Console.WriteLine("cuantos * quieres ver : ");
+            //int x = int.Parse(Console.ReadLine());
+            //for (int i = 0; i < x; i++)
+            //{
+            //    for (int j = 0; j < i + 1; j++)
+            //    {
+
+            //        Console.Write("* ");
+            //    }
+            //    Console.WriteLine();
+
+
+            //}
+
+            //Console.WriteLine("PATRON 1: ");
+            //Console.Write("filas: ");
+            //int y = int.Parse(Console.ReadLine());
+            //int number = 1;
+            //for (int i = 0; i < y; i++)
+            //{
+            //    for (int j = 0; j < i + 1; j++)
+            //    {
+
+            //        Console.Write(number++ + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+
+
+            //Console.WriteLine("PATRON 2: ");
+            //Console.Write("filas: ");
+            //int y = int.Parse(Console.ReadLine());
+            //int number = 1;
+            //for (int i = 0; i < y; i++)
+            //{
+            //    for (int j = 0; j < i + 1; j++)
+            //    {
+            //        Console.Write(number + " ");
+            //    }
+            //    number++;
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine("PATRON 3: ");
+            //Console.Write("filas: ");
+            //int y = int.Parse(Console.ReadLine());
+            //int number = 1;
+            //for (int i = 0; i < y; i++)
+            //{
+            //    for (int j = 0; j < i + 1; j++)
+            //    {
+            //        Console.Write(number + " ");
+            //        number++;
+            //    }
+            //    number = 1;
+            //    Console.WriteLine();
+            //}
+
+
+        }
+
+
+
+
+
+
+    }
+}
